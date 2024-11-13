763522f425f361adf6fcc20ecc51c4935e9c3b79
Random2127 <vmedina1978@gmail.com>
2024-11-13 23:38:22 +0100

    updated YouTubeDown

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ The folder "littleThings" will just have tests and usage of modules or practicin
 
 YTDownloader.py:
 Learning how to work with "yt_dlp" just a few tests to gather metadata, videos and audio.
-Still need to work on it. I'll make a menu for it, need to think about how I want it.
+Still need to work on it, i'll be noting down what I want to do and keep doing.
+~~- Made a basic console menu. ~~
+- Need to look into metadata which I would normally like to know
+- Read quality of videos and give the choice of which to download
+- Take the path where I'd like to save the file
+- Make a GUI for this in ¿React?
--- youtube/YTDownloader.py
+++ youtube/YTDownloader.py
@@ -1,48 +1,57 @@
 import yt_dlp
 
-url = "https://www.youtube.com/watch?v=igKXRrb5_ts"
+def downloadAudio(url):
+    ydl_opts = {
+        'outtmpl': '/home/victor/YTDowns/%(title)s.%(ext)s',
+        'format': 'bestaudio/best',         # Select quality
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',    # Using FFmpeg to extract audio
+            'preferredcodec': 'mp3',        # convert to .mp3
+            'preferredquality': '192',      # This is the Bitrate
+        }]
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
-
-# Learning to download ".mp3"
-
-
-ydl_opts = {
+def downloadVideo(url):
+    # I want to ask for path to save
+    ydl_opts = {
     'outtmpl': '/home/victor/YTDowns/%(title)s.%(ext)s',
-    'format': 'bestaudio/best',         # Select quality
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',    # Using FFmpeg to extract audio
-        'preferredcodec': 'mp3',        # convert to .mp3
-        'preferredquality': '192',      # This is the Bitrate
-    }],
-    
-}
+    'format': 'best' # This gets a .mp4 instead of .mkv
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
-
-
-
-
-# Learning to download ".mp4" videos 
-
-## Path to save the video
-#ydl_opts = {
-#    'outtmpl': '/home/victor/YTDowns/%(title)s.%(ext)s',
-#    'format': 'best' # This gets a .mp4 instead of .mkv
-#}
-## Create the object to interact with YT's video
-#with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-#    ydl.download([url])
-
-
-#Figuring out how to get metadata
-
-#with yt_dlp.YoutubeDL(ydl_ops) as ydl:
-#	# This line indicates only to download metadata
+# Metadata
+# with yt_dlp.YoutubeDL(ydl_ops) as ydl:
 #    info_dict = ydl.extract_info(url, download=False)
 #    print("Title: "+info_dict['title'])
 #    # Print all available metadata with keys to call it
 #    #for key, value in info_dict.items():
-#    #    print(f'{key}: {value}') # Not calling value as it complicates readability
-    
+#    #    print(f'{key}') # Not calling value as it complicates readability
 
+def main():
+    while (True):
+        print("Random Youtube downloader")        
+        print("1. Download video")
+        print("2. Download music")
+        print("3. Exit")
+        try:            
+            choice = int(input("Make a choice\n"))
+            if choice == 1:
+                url = input("Enter full URL:\n")
+                downloadVideo(url)
+            elif choice == 2:
+                url = input("Enter full URL:\n ")
+                downloadAudio(url)
+            elif choice == 3:
+                print("Bye")
+                break
+            else:
+                print("Option not available")
+        except ValueError:
+            print("Enter a number.")
+
+
+if __name__ == "__main__":
+    main()
